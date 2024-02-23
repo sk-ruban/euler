981dce4a5d31858dd1c195456e88c2de58e2fd03
Ruban <65346010+sk-ruban@users.noreply.github.com>
2024-02-23 20:45:51 +0800

    Add Problem 1

---
+++ 1.swift
@@ -0,0 +1,13 @@
+// Multiples of 3 or 5
+
+func multiples(_ first: Int, _ second: Int, _ limit: Int) -> Int {
+    var count = 0
+    for num in 1..<limit {
+        if num % first == 0  || num % second == 0 {
+            count += num
+        }
+    }
+    return count
+}
+
+print(multiples(3, 5, 1000))
