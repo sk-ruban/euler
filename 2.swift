95151412f0bdf4431d887cfd95906cd43ff6637d
Ruban <65346010+sk-ruban@users.noreply.github.com>
2024-02-23 20:56:06 +0800

    Add Problem 2

---
+++ 2.swift
@@ -0,0 +1,16 @@
+// Even Fibonacci Numbers
+
+func sum_even_fib(limit: Int) -> Int {
+    var count = 0
+    var (current, previous) = (1, 0)
+
+    while current < limit {
+        (current, previous) = (current + previous, current)
+        if current % 2 == 0 {
+            count += current
+        }
+    }
+    return count
+}
+
+print(sum_even_fib(limit: 4000000))
